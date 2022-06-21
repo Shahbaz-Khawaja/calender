b1b385016c9755110e72deeca312db041ee940f8
Shahbaz Khawaja <shahbaz.khawaja@techno-soft.com>
2022-06-21 13:22:27 +0500

    completed calendar almost

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,7 @@
 window.onload = () => {
-  document.querySelectorAll('.not-active').forEach(item => {
-      item.addEventListener('click', event => { 
-        highlight(event.currentTarget.id);
-      })
-    }); 
+  calender();
 }
+
 const date = new Date();
 const weekDays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
 const monthNames = [
@@ -26,25 +23,23 @@ const daysInCurrentMonth = (year, month) => {
   return new Date(year, month, 0).getDate();
 };
 
-const currentMonth = date.getMonth() + 1;
-const currentMonthName = monthNames[date.getMonth()];
-const currentYear = date.getFullYear();
+const calender = () => {
+  const currentMonth = date.getMonth() + 1;
+  const currentMonthName = monthNames[date.getMonth()];
+  const currentYear = date.getFullYear();
+  const todayIndex = date.getDay();
+  const lastDayIndex = new Date(currentYear, currentMonth, 0).getDay();
 
+  const currentmonthDays = daysInCurrentMonth(currentYear, currentMonth);
+  const prevMonthDays = daysInCurrentMonth(currentYear, currentMonth - 1);
+  const nextMonthDays = 7 - lastDayIndex;
 
-const calender = (currMonth, currMonthName, currYear) => {
-  const firstDayIndex = date.getDay();
-  const lastDayIndex = new Date(currYear, currMonth, 0).getDay();
-  
-  const monthDays = daysInCurrentMonth(currYear, currMonth);
-  const prevMonthDays = daysInCurrentMonth(currYear, currMonth - 1);
-
-  const nextDays = 7 - lastDayIndex;
   const weekDay = document.querySelector(".week-days");
   const monthDay = document.querySelector(".month-days");
 
   document.querySelector(
     ".month-year"
-  ).innerHTML = `<h3>${currMonthName} ${currentYear}</h3>`;
+  ).innerHTML = `<h3>${currentMonthName} ${currentYear}</h3>`;
 
   weekDay.innerHTML = ``;
   for (let i = 0; i < weekDays.length; i++) {
@@ -52,39 +47,43 @@ const calender = (currMonth, currMonthName, currYear) => {
   }
 
   monthDay.innerHTML = ``;
-  for(x = firstDayIndex; x > 0; x--){
-    monthDay.innerHTML +=`<h6 class="faint">${prevMonthDays - x + 1}</h6>`
+  for (x = todayIndex; x > 0; x--) {
+    monthDay.innerHTML += `<h6 class="faint">${prevMonthDays - x + 1}</h6>`;
   }
 
-  for (let i = 1; i <= monthDays; i++) {
-    if(i === date.getDate() && date.getMonth() === new Date().getMonth()){
-      monthDay.innerHTML += `<h6 class="not-active active" id="${i}">${i}</h6>`;  
-    }else{
-      monthDay.innerHTML += `<h6 class="not-active" id="${i}">${i}</h6>`;
-    }    
+  for (let i = 1; i <= currentmonthDays; i++) {
+    if (i === date.getDate() && date.getMonth() === new Date().getMonth()) {
+      monthDay.innerHTML += `<h6 class="not-active active" id="${i}${currentMonthName}">${i}</h6>`;
+    } else {
+      monthDay.innerHTML += `<h6 class="not-active" id="${i}${currentMonthName}">${i}</h6>`;
+    }
   }
 
-  for(let i = 1; i <= nextDays; i++){
-    monthDay.innerHTML +=`<h6 class="faint">${i}</h6>`
+  for (let i = 1; i <= nextMonthDays; i++) {
+    monthDay.innerHTML += `<h6 class="faint">${i}</h6>`;
   }
+
+  document.querySelectorAll('.not-active').forEach(item => {
+    item.addEventListener('click', event => {
+      highlight(event.currentTarget.id);
+    })
+  });
 };
 
 const nextMonth = () => {
   date.setMonth(date.getMonth() + 1);
-  const currentMonthName = monthNames[date.getMonth()];
-  calender(date.getMonth() + 1, currentMonthName, currentYear);
+  calender();
 };
 
 const prevMonth = () => {
   date.setMonth(date.getMonth() - 1);
-  const currentMonthName = monthNames[date.getMonth()];
-  calender(date.getMonth() + 1, currentMonthName, currentYear);
+  calender();
 };
 
-calender(currentMonth, currentMonthName, currentYear);
-
 const highlight = (elementId) => {
-  console.log(`as${elementId}`);
-  document.querySelector('.active').classList.remove('active'); 
-  document.getElementById(elementId).classList.add('active');
-}
+  let isActiveExist = document.getElementsByClassName('active');
+  if(isActiveExist.length) {
+    document.querySelector(".active").classList.remove("active");
+  }
+  document.getElementById(elementId).classList.add("active");
+};
